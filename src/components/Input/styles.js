c0544cf323bd761b8af8b89378478c7dbda5b1e3
lilianlacerda <lilianraiany@gmail.com>
2026-01-10 21:16:29 -0300

    add: estilização do input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+
+    input{
+        background-color: #55c6e9ff;
+        color: #333333;
+        font-weight: 700;
+        font-family: 'Roboto';
+        width: 100%;
+        height: 80px;
+        border-radius: 16px;
+        box-shadow: 0px 10px 25px rgba(0,0,0,0.15);
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 30px;
+    }
+
+`
+
